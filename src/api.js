d648d54899df4b478fb6d93c3e01e818f9938a8d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-20 05:58:22 -0700

    Give location input to get weather data

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 // api.js
 
-async function getWeatherData(){
+async function getWeatherData(location = 'london'){
+    const locationParameter = location ? encodeURIComponent(location) : 'london';
     try {
         const response = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationParameter}?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
         const responseData = await response.json();
         return responseData;
     
