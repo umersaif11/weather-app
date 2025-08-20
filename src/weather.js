3805a0308cd588f80f81b0cb2c6ee3d74c913f9a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-20 06:28:52 -0700

    Return the refined needed properties into an object

--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,20 @@
 // weather.js
-import {getWeatherData} from './api.js'
+import { getWeatherData } from './api.js'
 
-async function processWeatherData() {
-    const result = await getWeatherData();
+async function processWeatherPrimaryData(location) {
+    const result = await getWeatherData(location);
     return {
         city: result.address, 
         description: result.description,
+        temperature: result.currentConditions.temp,
+        sunrise: result.currentConditions.sunrise,
+        sunset: result.currentConditions.sunset,
+        moonphase: result.currentConditions.moonphase,
+        todayoverview: result.days[0],
+        feelslike: result.currentConditions.feelslike,
+        precipitation: result.currentConditions.precip,
+        windspeed: result.currentConditions.windspeed,
+        humidity: result.currentConditions.humidity,
     }
 }
-export{ processWeatherData }
+export{ processWeatherPrimaryData }
