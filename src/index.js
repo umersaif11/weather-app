15838bee178a16453a7015969fc852bdc415db1a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-20 00:58:22 -0700

    if response status is 404 then throw an error

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ async function getWeather(){
         const response = await fetch(
             `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
         console.log(response);
-        
+        if(!response.ok){
+            throw new Error('Whops! Error:', response.status);
+        }
     }
     catch {
 
