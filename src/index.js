82d076ab625c48357706433d178b26144d517ca3
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-20 03:29:59 -0700

    Return the response in json form as promise from the getweather data function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 // index.js
 import "./styles.css";
 
-async function getWeather(){
+async function getWeatherData(){
     try {
         const response = await fetch(
             `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
         console.log(response);
-        if(!response.ok){
-            throw new Error('Whops! Error:', response.status);
-        }
+        // if(!response.ok){
+        //     throw new Error('Whops! Error:', response.status);
+        // }
         const responseData = await response.json();
         console.log(responseData);
+        return responseData;
+       
     }
     catch(error) {
         console.log('Network error:', error);
     }
 }
-getWeather();
+console.log(getWeatherData());
   
