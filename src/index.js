1c7e787a0e0b7e5f2e0f0f0d4900da7eced7912f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-20 03:40:33 -0700

    Make the factory function weather info

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 // index.js
 import "./styles.css";
 
-async function getWeatherData(){
-    try {
-        const response = await fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
-        console.log(response);
-        // if(!response.ok){
-        //     throw new Error('Whops! Error:', response.status);
-        // }
-        const responseData = await response.json();
-        console.log(responseData);
-        return responseData;
-       
+const WeatherInfo = () => {
+    async function getWeatherData(){
+        try {
+            const response = await fetch(
+                `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=2VQ3QPNM3QNQE6SW7FCVEMYRT`);
+            console.log(response);
+            // if(!response.ok){
+            //     throw new Error('Whops! Error:', response.status);
+            // }
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+        
+        }
+        catch(error) {
+            console.log('Network error:', error);
+        }
     }
-    catch(error) {
-        console.log('Network error:', error);
+    console.log(getWeatherData());
+    const getNeededWeatherData = () => {
+        
     }
+
 }
-console.log(getWeatherData());
+WeatherInfo();
   
