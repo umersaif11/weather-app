60e269568f68746f537537445e70fe7a47942ec0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-21 01:08:47 -0700

    Similarly for upcoming days get the required attributes and push them into empty array using loop

--- src/weather.js
+++ src/weather.js
@@ -4,10 +4,20 @@ import { getWeatherData } from './api.js'
 async function processWeatherData(location) {
     const result = await getWeatherData(location);
     const todayOverview = [];
+    const upcomingDays = [];
+    const sevenUpcomingDays = result.days.slice(0,8);
     for(let i = 0; i < result.days[0].hours.length; i++){
         todayOverview.push({
             time: result.days[0].hours[i].datetime,
             temperature: result.days[0].hours[i].temp
+        })
+    }
+    for(let i = 0; i < sevenUpcomingDays.length; i++){
+        upcomingDays.push({
+            date: sevenUpcomingDays[i].datetime,
+            tempmax: sevenUpcomingDays[i].tempmax,
+            tempmin: sevenUpcomingDays[i].tempmin,
+            conditions: sevenUpcomingDays[i].conditions
         })
     }
     return {
