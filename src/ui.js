2b3ec2b2e02d782d9a0c46cb7aebdb93027d58de
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-29 05:17:21 -0700

    For testing purposes just take out temperature and city from the function for now

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,9 @@
 //ui.js
 function renderWeather(weatherData){
-
+    const app = document.getElementById('app');
+    app.innerHTML = `
+    <h2>${weatherData.city}</h2>
+    <p>Current Temp: ${weatherData.temperature.f} °F</p>
+    `
 }
 export { renderWeather }
