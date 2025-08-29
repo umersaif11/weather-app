7cfd65988054f876a0a4b154b198d24b60bc4775
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-29 05:20:06 -0700

    Import render weather function and give refined object as argument and so our temp and city will appear on page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // index.js
 import "./styles.css";
 import { processWeatherData } from "./weather.js";
+import { renderWeather } from "./ui.js"
 
 const refinedDataObject = await processWeatherData("hafizabad");
 console.log(refinedDataObject);
+renderWeather(refinedDataObject);
