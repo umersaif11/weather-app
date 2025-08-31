69ceb23fc9b220e76102da3dea102639b173ddb0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-31 06:34:47 -0700

    Add the element for city name in the renderweather

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,5 @@
 //ui.js
-function renderWeather(weatherData){
+function renderWeather(weatherData, displayCityName){
     const app = document.getElementById('app');
     app.innerHTML = '';
 
@@ -7,6 +7,15 @@ function renderWeather(weatherData){
     currentForecast.classList.add('currentForecast');
     const header = document.createElement('div');
     header.classList.add('location');
+
+    const headerLocation = document.createElement('h2');
+    headerLocation.classList.add('locationHead');
+    headerLocation.textContent = displayCityName;
+
+    header.appendChild(
+        headerLocation
+    )
+
     currentForecast.appendChild(header);
 
     app.appendChild(currentForecast);
