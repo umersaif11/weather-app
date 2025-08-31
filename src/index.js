406efb19cd568062ae1d0b71317ab87e1d97ba32
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-31 06:33:52 -0700

    Updated renderweather function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import "./styles.css";
 import { processWeatherData } from "./weather.js";
 import { renderWeather } from "./ui.js"
 
-const refinedDataObject = await processWeatherData("hafizabad");
+const refinedDataObject = await processWeatherData("sydney");
 console.log(refinedDataObject);
-renderWeather(refinedDataObject);
+renderWeather(refinedDataObject, 'Sydney');
