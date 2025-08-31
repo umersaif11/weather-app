4d98b48838e187e941e80bbfa9504fcfe9a5cba3
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-31 06:14:59 -0700

    Within current forecast div, create a div as header

--- src/ui.js
+++ src/ui.js
@@ -5,5 +5,12 @@ function renderWeather(weatherData){
 
     const currentForecast = document.createElement('div');
     currentForecast.classList.add('currentForecast');
+    const header = document.createElement('div');
+    header.classList.add('location');
+    currentForecast.appendChild(header);
+
+    app.appendChild(currentForecast);
+
+
 }
 export { renderWeather }
