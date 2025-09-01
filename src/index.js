0a51dc7adaba8f68a14a623365366fb84a31c78f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-01 04:12:16 -0700

    Remove the locationIQ testing snippet

--- src/index.js
+++ src/index.js
@@ -6,13 +6,3 @@ import { renderWeather } from "./ui.js"
 const refinedDataObject = await processWeatherData("sydney");
 console.log(refinedDataObject);
 renderWeather(refinedDataObject, 'Sydney');
-
-async function getLocationData(location){
-  const encodedLocation = encodeURIComponent(location);
-  const response = await fetch(
-    `https://us1.locationiq.com/v1/search?key=pk.878c1fdb1c7c62477f111ba0d35333bf&q=${encodedLocation}&format=json&limit=1&namedetails=1&addressdetails=1`
-  );
-  console.log(response)
-  
-}
-getLocationData('sydney');
