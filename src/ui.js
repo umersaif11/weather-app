5a469934e7c482d0538186e097b306735fd70fe3
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-02 03:35:36 -0700

    Make the function to get the moon phase names for each value of moon phase

--- src/ui.js
+++ src/ui.js
@@ -74,6 +74,16 @@ const weatherImageMap = {
 function getWeatherIconClass(icon) {
     return weatherIconMap[icon] || 'wi-na';
 }
+function getMoonPhaseName(value) {
+    if (value === 0) return 'New Moon';
+    if (value > 0 && value < 0.25) return 'Waxing Crescent';
+    if (value === 0.25) return 'First Quarter';
+    if (value > 0.25 && value < 0.5) return 'Waxing Gibbous';
+    if (value === 0.5) return 'Full Moon';
+    if (value > 0.5 && value < 0.75) return 'Waning Gibous';
+    if (value === 0.75) return 'Last Quarter';
+    return 'Waning Crescent';
+}
 function renderWeather(weatherData){
     const app = document.getElementById('app');
     app.innerHTML = '';
