ea386f70cb919f779419f21deffae50fbfa7c9ea
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-09 02:11:12 -0700

    Removed some testing code

--- src/index.js
+++ src/index.js
@@ -6,16 +6,3 @@ import { renderWeather } from "./ui.js"
 const refinedDataObject = await processWeatherData("hafizabad, pakistan");
 console.log(refinedDataObject);
 renderWeather(refinedDataObject);
-async function setLocationBackground(query) {
-    const response = await fetch(
-      `https://api.pexels.com/v1/search?query=${encodeURIComponent(query)}&per_page=1&orientation=landscape`,
-      {
-        headers: {
-          Authorization: 'RBhECvORcQjs8GUlW3FXmy7WwczHde6Tbhke2hr2tWRyY4acANLRq6A4',
-        }
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-}
-setLocationBackground('sun rise');
