7780ff3982bacbcc813306ef22f10eee169ca40b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-11 11:41:58 +0500

    Initialize the currentUnit equal to celcius and a variable to store the weather data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles.css";
 import { processWeatherData } from "./weather.js";
 import { renderWeather } from "./ui.js"
 
+let currentUnit = 'metric';
+let currentWeatherData = null;
+
 const refinedDataObject = await processWeatherData("hafizabad, pakistan");
 console.log(refinedDataObject);
-renderWeather(refinedDataObject, 'metric');
+renderWeather(refinedDataObject, currentUnit);
